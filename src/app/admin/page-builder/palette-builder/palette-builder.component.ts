88851c2c9c06a4c41790f4bc1af51d49d859a4fc
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2020-08-14 22:11:03 +0200

    add btesting pallete items

--- src/app/admin/page-builder/palette-builder/palette-builder.component.ts
+++ src/app/admin/page-builder/palette-builder/palette-builder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-palette-builder',
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaletteBuilderComponent implements OnInit {
 
-  constructor() { }
+  baseBlocks: { image: string, id: number }[];
+
+  constructor() {
+    this.baseBlocks = [
+      {image: 'https://via.placeholder.com/300/000458?text=2', id: 1},
+      {image: 'https://via.placeholder.com/300/0086985?text=5', id: 2}
+    ];
+  }
+
+  drop(event: CdkDragDrop<{ image: string, id: number }[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 
   ngOnInit(): void {
   }
