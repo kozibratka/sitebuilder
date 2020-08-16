f04a57ddbcc7317a3c4a1a7a6614daf8fc97f6dc
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2020-08-16 22:21:28 +0200

    scroll directive

---
+++ src/app/admin/page-builder/directives/smart-page-scroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SmartPageScrollDirective } from './smart-page-scroll.directive';
+
+describe('SmartPageScrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SmartPageScrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/admin/page-builder/directives/smart-page-scroll.directive.ts
@@ -0,0 +1,18 @@
+import {Directive, ElementRef, HostListener, NgZone} from '@angular/core';
+
+@Directive({
+  selector: '[appSmartPageScroll]'
+})
+export class SmartPageScrollDirective {
+
+  constructor(private window: Window, private element: ElementRef, private zone: NgZone) {
+    this.zone.runOutsideAngular(() => {
+      this.element.nativeElement.addEventListener('drag', this.doScroll.bind(this));
+    });
+  }
+
+  private doScroll(): void{
+    console.log("hejbu");
+  }
+
+}
