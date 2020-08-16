eba6e2e5ea3d34a37e397caeb854dc4a0204197a
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2020-08-16 22:19:18 +0200

    scroll directive

--- src/app/admin/page-builder/page-builder.module.ts
+++ src/app/admin/page-builder/page-builder.module.ts
@@ -5,16 +5,18 @@ import { MenuBuilderComponent } from './menu-builder/menu-builder.component';
 import { PaletteBuilderComponent } from './palette-builder/palette-builder.component';
 import { PageBuilderRoutingModule } from './page-builder-routing.module';
 import {SortablejsModule} from 'ngx-sortablejs';
+import { SmartPageScrollDirective } from './directives/smart-page-scroll.directive';
 
 
 
 @NgModule({
-  declarations: [PageBuilderComponent, MenuBuilderComponent, PaletteBuilderComponent],
+  declarations: [PageBuilderComponent, MenuBuilderComponent, PaletteBuilderComponent, SmartPageScrollDirective],
   imports: [
     CommonModule,
     PageBuilderRoutingModule,
     SortablejsModule
   ],
-  exports: [PageBuilderComponent]
+  exports: [PageBuilderComponent],
+  providers: [{provide: Window, useValue: window}]
 })
 export class PageBuilderModule { }
