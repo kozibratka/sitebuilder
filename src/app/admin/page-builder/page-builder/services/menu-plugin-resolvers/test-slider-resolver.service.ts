b5a6e86dda2a7926ef72945af6c8e9951e3082ab
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2020-08-28 22:53:01 +0200

    remove injectable from component service

--- src/app/admin/page-builder/page-builder/services/menu-plugin-resolvers/test-slider-resolver.service.ts
+++ src/app/admin/page-builder/page-builder/services/menu-plugin-resolvers/test-slider-resolver.service.ts
@@ -1,9 +1,5 @@
-import {Injectable} from '@angular/core';
 import {AbstractMenuPluginResolver} from './abstract-menu-plugin-resolver';
 
-@Injectable({
-  providedIn: 'root'
-})
 export class TestSliderResolverService extends AbstractMenuPluginResolver {
 
   constructor() {
