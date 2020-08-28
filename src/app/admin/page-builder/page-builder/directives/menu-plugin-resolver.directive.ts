4ebdc4949187e3ae5cd2ec403559653c041d831a
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2020-08-28 22:49:37 +0200

    component from menu resolver

--- src/app/admin/page-builder/page-builder/directives/menu-plugin-resolver.directive.ts
+++ src/app/admin/page-builder/page-builder/directives/menu-plugin-resolver.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import {Directive, HostListener, Inject} from '@angular/core';
+import {MenuPluginResolverMessengerService} from '../services/menu-plugin-resolver-messenger.service';
+import {AbstractMenuPluginResolver} from '../services/menu-plugin-resolvers/abstract-menu-plugin-resolver';
 
 @Directive({
   selector: '[appMenuPluginResolver]'
 })
 export class MenuPluginResolverDirective {
 
-  constructor() { }
+  @Inject(MenuPluginResolverMessengerService) menuPluginResolverMessengerService: MenuPluginResolverMessengerService;
 
+  constructor(private abstractMenuPluginResolver: AbstractMenuPluginResolver) {
+
+  }
+
+  @HostListener('dragstart')
+  resolveComponent(): void{
+    this.menuPluginResolverMessengerService.setComponentToCreate(this.abstractMenuPluginResolver.getComponent());
+  }
 }
--- src/app/admin/page-builder/page-builder/page-builder.component.ts
+++ src/app/admin/page-builder/page-builder/page-builder.component.ts
@@ -1,9 +1,12 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {TestSliderResolverService} from './services/menu-plugin-resolvers/test-slider-resolver.service';
+import {AbstractMenuPluginResolver} from './services/menu-plugin-resolvers/abstract-menu-plugin-resolver';
 
 @Component({
   selector: 'app-page-builder',
   templateUrl: './page-builder.component.html',
-  styleUrls: ['./page-builder.component.css']
+  styleUrls: ['./page-builder.component.css'],
+  providers: [{provide: AbstractMenuPluginResolver, useClass: TestSliderResolverService, multi: true }]
 })
 export class PageBuilderComponent implements OnInit, AfterViewInit {
 
