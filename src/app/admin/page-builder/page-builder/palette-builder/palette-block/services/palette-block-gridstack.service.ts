d6cd3008e4f7b2b24f9f591f5a2b862d8bc88c6d
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2020-09-18 19:58:05 +0200

    Custom drag&drop mdificator

--- src/app/admin/page-builder/page-builder/palette-builder/palette-block/services/palette-block-gridstack.service.ts
+++ src/app/admin/page-builder/page-builder/palette-builder/palette-block/services/palette-block-gridstack.service.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectorRef, ElementRef, Host, Inject, Injectable, NgZone} from '@angular/core';
 import {GridStack, GridStackNode} from 'gridstack/dist/gridstack';
+import {GridStackDragDrop} from '../3rd-party-modificators/grid-stack-drag-drop';
 
 @Injectable()
 export class PaletteBlockGridstackService {
@@ -21,37 +22,25 @@ export class PaletteBlockGridstackService {
 
   startGridstack(): void {
     this.zone.runOutsideAngular(() => {
-      if (this.gridStack) {
-        this.gridStack.destroy(false);
-      }
       this.gridStack = GridStack.init({
         acceptWidgets: true,
         column: 12,
+        ddPlugin: GridStackDragDrop,
         float: true,
         styleInHead: true,
         placeholderText: "Zde bude nový obsah :)",
-        staticGrid: true
       }, this.gridstackElement.nativeElement);
       this.isInited = true;
-      this.gridStack.setStatic(false);
       (this.gridStack as any).on('dropped', (event: Event, arg2: any, arg3: any) => {
         this.gridStack.removeWidget(arg3.el);
         this.gridStackNodes.push(arg3);
         this.changeDetectorRef.detectChanges();
       });
-      (this.gridStack as any).off('dropped');
     });
-  }
-
-  terminate(): void{
-    // this.gridStack.destroy(false);
-    // setTimeout(() => this.startGridstack(), 0);
-
   }
 
   addWidget(elementRef: ElementRef): void {
     if (this.isInited) {
-      console.log("add");
       this.gridStack.addWidget(elementRef.nativeElement);
     }
   }
