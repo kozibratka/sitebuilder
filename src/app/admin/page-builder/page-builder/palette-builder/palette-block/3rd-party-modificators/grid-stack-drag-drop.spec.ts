0a363773b9a311c41c138333ac4a4257fccf7087
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2020-09-18 19:58:19 +0200

    Custom drag&drop mdificator

---
+++ src/app/admin/page-builder/page-builder/palette-builder/palette-block/3rd-party-modificators/grid-stack-drag-drop.spec.ts
@@ -0,0 +1,7 @@
+import { GridStackDragDrop } from './grid-stack-drag-drop';
+
+describe('GridStackDragDrop', () => {
+  it('should create an instance', () => {
+    expect(new GridStackDragDrop()).toBeTruthy();
+  });
+});
---
+++ src/app/admin/page-builder/page-builder/palette-builder/palette-block/3rd-party-modificators/grid-stack-drag-drop.ts
@@ -0,0 +1,19 @@
+import {GridStackDDJQueryUI, $} from 'gridstack';
+
+export class GridStackDragDrop extends GridStackDDJQueryUI{
+  on(el, name, callback): GridStackDragDrop {
+    const $el = $(el);
+    $el.on(name, (event, ui) => {
+      if (!ui){
+        return;
+      }
+      callback(event, ui.draggable ? ui.draggable[0] : event.target, ui.helper ? ui.helper[0] : null);
+    });
+    return this;
+  }
+  off(el, name): GridStackDragDrop {
+    const $el = $(el);
+    $el.off(name);
+    return this;
+  }
+}
