3e75b4ea1ff8ceba2a4428a51a5b5ff65f0b10c8
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2020-09-26 22:16:14 +0200

    plugin item resolver

--- src/app/admin/page-builder/page-builder/palette-builder/palette-block/palette-item/palette-item.component.ts
+++ src/app/admin/page-builder/page-builder/palette-builder/palette-block/palette-item/palette-item.component.ts
@@ -1,6 +1,17 @@
-import {AfterViewInit, Component, ElementRef, Input, OnInit} from '@angular/core';
+import {
+  AfterViewInit,
+  Component, ComponentFactory,
+  ComponentFactoryResolver, ComponentRef,
+  ElementRef,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
 import {GridStackNode} from 'gridstack';
 import {PaletteBlockGridstackService} from '../services/palette-block-gridstack.service';
+import {MenuPluginResolverService} from '../../../menu-builder/services/menu-plugin-resolvers/menu-plugin-resolver.service';
 
 @Component({
   selector: 'app-palette-item',
@@ -10,8 +21,15 @@ import {PaletteBlockGridstackService} from '../services/palette-block-gridstack.
 export class PaletteItemComponent implements OnInit, AfterViewInit {
 
   @Input() gridStackNode: GridStackNode;
+  @ViewChild(ViewContainerRef) viewContainerRef: ViewContainerRef;
+  private componentRef: ComponentRef<any>;
 
-  constructor(private paletteBlockGridstackService: PaletteBlockGridstackService, private elementRef: ElementRef) {
+  constructor(
+    private paletteBlockGridstackService: PaletteBlockGridstackService,
+    private elementRef: ElementRef,
+    private menuPluginResolverService: MenuPluginResolverService,
+    private resolver: ComponentFactoryResolver
+  ) {
 
   }
 
@@ -26,16 +44,22 @@ export class PaletteItemComponent implements OnInit, AfterViewInit {
     return this.elementRef;
   }
 
-  getHeightInGrid(): number{
+  getHeightInGrid(): number {
     return this.getelementRef().nativeElement.gridstackNode.height;
   }
 
-  getYPositionInGrid(): number{
+  getYPositionInGrid(): number {
     return this.getelementRef().nativeElement.gridstackNode.y;
   }
 
   getRowsInGrid(): number {
     return this.getHeightInGrid() + this.getYPositionInGrid();
+  }
+
+  createPluginFromMenu(): void {
+    const factory: ComponentFactory<any> = this.resolver.resolveComponentFactory(
+      this.menuPluginResolverService.selectedAbstractMenuPluginResolverMessenger.componentClass);
+    this.componentRef = this.viewContainerRef.createComponent<any>(factory);
   }
 
 }
