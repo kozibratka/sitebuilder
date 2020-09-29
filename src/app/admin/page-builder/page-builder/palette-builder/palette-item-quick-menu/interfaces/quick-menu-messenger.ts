c472e12289d19b4091694b4c0ac3ca5bd013f56e
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2020-09-29 19:50:53 +0200

    quick menu animation

---
+++ src/app/admin/page-builder/page-builder/palette-builder/palette-item-quick-menu/interfaces/quick-menu-messenger.ts
@@ -0,0 +1,6 @@
+import {PaletteItemComponent} from '../../palette-block/palette-item/palette-item.component';
+
+export interface QuickMenuMessenger {
+  mouseEvent: MouseEvent;
+  paletteItemComponent: PaletteItemComponent;
+}
