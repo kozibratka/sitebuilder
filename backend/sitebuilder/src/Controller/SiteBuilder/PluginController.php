aa082262db12a32388d06da1d426eea9db31256a
Martin Kozibrátka <martin.kozibratka@bohemiasoft.com>
2021-02-19 21:47:49 +0100

    plugin as global

---
+++ backend/sitebuilder/src/Controller/SiteBuilder/PluginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\SiteBuilder;
+
+use App\Controller\BaseApiController;
+use App\Entity\SiteBuilder\Plugin\BasePlugin;
+use App\Entity\SiteBuilder\Web;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("plugin", name="plugin_")
+ */
+class PluginController extends BaseApiController
+{
+    /**
+     * @Route("/list-global/{id}", name="list")
+     */
+    public function listGlobal(Web $web)
+    {
+        $this->denyAccessUnlessGranted('page_builder_voter',$web);
+        $globalPlugins = $this->getDoctrine()->getRepository(BasePlugin::class)->findBy(['web' => $web]);
+        return $this->jsonResponseSimple($globalPlugins);
+    }
+}
