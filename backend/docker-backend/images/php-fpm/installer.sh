fe38d0438cffbea2fb647d2fb1ffcbfcc34896b0
Martin Kozibrátka <martin.kozibratka@gmail.com>
2021-04-12 20:14:03 +0200

    update docker compose

--- backend/docker-backend/images/php-fpm/installer.sh
+++ backend/docker-backend/images/php-fpm/installer.sh
@@ -7,7 +7,6 @@ docker-php-ext-enable pdo_mysql
 
 if [ "$NODE_ENV" == "dev-local" ]; then
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-  php -r "if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
   php composer-setup.php
   php -r "unlink('composer-setup.php');"
   mv composer.phar /usr/local/bin/composer
