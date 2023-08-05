e8ba834f490ae31b6142d58f1a6d58e1d836ac3b
Martin Kozibrátka <martin.kozibratka@starnet.cz>
2023-08-05 10:07:03 +0200

    2

--- backend/sitebuilder/src/Form/SiteBuilder/Plugin/Carousel/CarouselV1/CarouselV1Type.php
+++ backend/sitebuilder/src/Form/SiteBuilder/Plugin/Carousel/CarouselV1/CarouselV1Type.php
@@ -4,6 +4,7 @@
 namespace App\Form\SiteBuilder\Plugin\Carousel\CarouselV1;
 
 
+use App\Entity\SiteBuilder\Plugin\Carousel\CarouselV1\CarouselV1;
 use App\Form\SiteBuilder\Plugin\BasePluginType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -20,7 +21,7 @@ class CarouselV1Type extends AbstractType
             ->add('intervalRotate', TextType::class)
             ->add('autostart', CheckboxType::class)
             ->add('images', CollectionType::class, [
-                'entry_type' => CarouselBootstrapImageType::class,
+                'entry_type' => CarouselV1ImageType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
                 'by_reference' => false,
@@ -31,7 +32,7 @@ class CarouselV1Type extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => CarouselBootstrapPlugin::class,
+            'data_class' => CarouselV1::class,
         ]);
     }
 
