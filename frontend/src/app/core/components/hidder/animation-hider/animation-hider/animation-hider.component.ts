b29f58b1076d1c9140abc3f03242eb5e31c14eb9
Martin Kozibrátka <martin.kozibratka@starnet.cz>
2023-09-12 21:19:18 +0200

    2

--- frontend/src/app/core/components/hidder/animation-hider/animation-hider/animation-hider.component.ts
+++ frontend/src/app/core/components/hidder/animation-hider/animation-hider/animation-hider.component.ts
@@ -9,9 +9,11 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
     trigger('showHide', [
       state('show', style({
         opacity: 1,
+        display: 'block',
       })),
       state('hide', style({
         opacity: 0,
+        display: 'none',
       })),
       transition('show => hide', [
         animate('0.5s')
--- frontend/src/app/core/modules/form-builder/form-builder.module.ts
+++ frontend/src/app/core/modules/form-builder/form-builder.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormBuilderComponent } from './components/form-builder/form-builder.component';
 import { FormInputComponent } from './components/form-input/form-input.component';
 import {CoreModule} from '../../core.module';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
@@ -16,7 +19,10 @@ import {CoreModule} from '../../core.module';
   ],
   imports: [
     CommonModule,
-    CoreModule
+    CoreModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ]
 })
 export class FormBuilderModule { }
