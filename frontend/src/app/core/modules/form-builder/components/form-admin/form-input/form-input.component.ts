cd9a4bd21b29d3c6c65343c0e3fe19aed6b0319c
Martin Kozibrátka <martin.kozibratka@starnet.cz>
2023-09-30 19:30:34 +0200

    2

--- frontend/src/app/core/modules/form-builder/components/form-admin/form-input/form-input.component.ts
+++ frontend/src/app/core/modules/form-builder/components/form-admin/form-input/form-input.component.ts
@@ -6,7 +6,7 @@ import {StringService} from '../../../../../services/string.service';
 @Component({
   selector: 'app-form-input',
   templateUrl: './form-input.component.html',
-  styleUrls: ['./form-input.component.css']
+  styleUrls: ['./form-input.component.css'],
 })
 export class FormInputComponent {
   @Input() formData: any;
