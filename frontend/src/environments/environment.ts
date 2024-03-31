65f05ab4b7772e7a50cd7c816b6730553c72af0d
Martin Kozibrátka <martin.kozibratka@starnet.cz>
2024-03-31 22:14:26 +0200

    TEst

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'http://127.0.0.1:8000',
+  backendUrl: 'http://127.0.0.1:8080',
   backendRoutesPath: '/routes/fos_js_routes.json',
   hostname: 'www.sitebuilder.cz',
   previewSubdomain: 'preview',
