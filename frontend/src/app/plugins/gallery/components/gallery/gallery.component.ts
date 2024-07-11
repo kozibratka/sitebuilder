7622f5402978573bbbcfb78094de5b07c81a52fe
Martin Kozibrátka <martin.kozibratka@starnet.cz>
2024-07-12 00:18:59 +0200

    Test

--- frontend/src/app/plugins/gallery/components/gallery/gallery.component.ts
+++ frontend/src/app/plugins/gallery/components/gallery/gallery.component.ts
@@ -1,24 +1,51 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {AbstractPlugin} from "../../../shared/abstract-class/abstract-plugin";
 import {GalleryConfigInterface} from "../../interfaces/gallery-config-interface";
+import {CommonModule} from "@angular/common";
+(window as any).global = {};
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
-export class GalleryComponent extends AbstractPlugin<GalleryConfigInterface> implements OnInit {
+export class GalleryComponent extends AbstractPlugin<GalleryConfigInterface> implements OnInit, AfterViewInit, OnDestroy {
 
+  @ViewChild('content', {static: true}) content: ElementRef;
+  gallery = null;
   constructor() {
     super();
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
     this.refreshView();
   }
 
   refreshView(): void {
+    if (this.gallery) {
+      ($(this.content.nativeElement) as any).nanogallery2('destroy');
+    }
+    let images = this.settings.images.map(value => {
+      return {src: value.path, srct: value.path, title: value.h1}
+    });
+
+    this.gallery = ($(this.content.nativeElement) as any).nanogallery2({
+      items:images,
+      thumbnailWidth:  'auto',
+      thumbnailHeight: 100,
+
+      locationHash:    false
+    });
+  }
+
+  ngOnDestroy(): void {
   }
 }
