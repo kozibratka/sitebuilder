2f5e2e75dbdbcf45ae0071e6b888c3854ab45bbb
Martin Kozibrátka <martin.kozibratka@starnet.cz>
2024-07-14 23:31:56 +0200

    Test

--- frontend/src/app/plugins/gallery/components/gallery/gallery.component.ts
+++ frontend/src/app/plugins/gallery/components/gallery/gallery.component.ts
@@ -30,12 +30,20 @@ export class GalleryComponent extends AbstractPlugin<GalleryConfigInterface> imp
   }
 
   refreshView(): void {
+    let originSize = '';
     if (this.gallery) {
+      if (!this.content.nativeElement.style.height) {
+        originSize = this.content.nativeElement.offsetHeight.toString()+'px';
+      }
+      if (originSize) {
+        this.content.nativeElement.style.height = originSize;
+      }
       ($(this.content.nativeElement) as any).nanogallery2('destroy');
     }
     let images = this.settings.images.map(value => {
       return {src: value.path, srct: value.path, title: value.h1}
     });
+
     this.gallery = ($(this.content.nativeElement) as any).nanogallery2({
       items:images,
       thumbnailWidth:  'auto',
@@ -43,6 +51,18 @@ export class GalleryComponent extends AbstractPlugin<GalleryConfigInterface> imp
       thumbnailAlignment: 'right',
       locationHash:    false,
       thumbnailDisplayTransition: "scaleUp",
+      fnThumbnailInit: ($thumbnail, item, GOMidx) => {
+        if (originSize && GOMidx+1 == this.settings.images.length) {
+          let images = this.content.nativeElement.querySelectorAll('img');
+          if (!images.length) {
+            return;
+          }
+          let lastImage = images[images.length-1];
+          lastImage.addEventListener('load', ev => {
+            this.content.nativeElement.style.height = '';
+          });
+        }
+      }
     });
   }
 
