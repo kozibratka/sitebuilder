09e1c4a1336c3083ba9f7f681cbbc14ed785b3b0
aloha <martin.kozibratka@gmail.com>
2024-08-23 22:44:00 +0200

    Upgrade

--- frontend/src/app/website/pages/preview/preview.component.ts
+++ frontend/src/app/website/pages/preview/preview.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SystemInfoService} from "../../../core/services/system-info.service";
 
 @Component({
   selector: 'app-preview',
@@ -9,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class PreviewComponent {
 
+  constructor(
+    public systemInfoService: SystemInfoService,
+  ) {
+  }
 }
